67786da0a8756c0fea3a8d25e9dc701f84190c78
BasilMawejje <basil.mawejje@gmail.com>
2019-10-07 20:20:18 +0300

    Add articles controller

--- app/api/v1/controllers/articles_controller.rb
+++ app/api/v1/controllers/articles_controller.rb
@@ -1,5 +1,15 @@
 require 'sinatra/base'
 require 'sinatra/activerecord'
+require './app/api/v1/models/article.rb'
 
 class ArticlesController < ApplicationController
+  get '/articles' do
+    articles = Article.all
+    articles.to_json({:only => [:id, :title, :post, :category_id]})
+  end
+
+  get '/articles/:id' do
+    article = Article.find(params[:id])
+    article.to_json({:only => [:id, :title, :post, :category_id]})
+  end
 end
