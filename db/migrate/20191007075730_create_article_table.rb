dd04d5b436defb8d63264934f63ec6a522324511
BasilMawejje <basil.mawejje@gmail.com>
2019-10-07 11:01:04 +0300

    Setup and initialise database

---
+++ db/migrate/20191007075730_create_article_table.rb
@@ -0,0 +1,10 @@
+class CreateArticleTable < ActiveRecord::Migration[6.0]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.string :post
+      t.datetime :created_at
+      t.datetime :updated_at
+    end
+  end
+end
